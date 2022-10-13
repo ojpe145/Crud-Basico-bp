23873048d0f1176839585d3c68aba2230262d887
ojpe145 <osnaider_pacheco14@hotmail.com>
2022-10-12 19:55:31 -0500

    index limpio

---
+++ src/components/Variables.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Variables = () => {
+    const saludo = "Hola mundo";
+    const imagen = 'https://picsum.photos/300'
+    const texto_alt = 'Esto es una imagen aleatoria de picsum'
+  return (
+    <div>
+        <h1>{saludo}</h1>
+        <img src={imagen} alt={texto_alt} />
+    </div>
+  )
+}
+
+export default Variables
